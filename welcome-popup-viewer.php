0173bc55f9b31259ee9d3677a2fd0c57b927d701
Habibul Islam <habibulislam6862@gmail.com>
2023-07-04 05:27:15 +0600

    welcome-popup-viewer init

---
+++ welcome-popup-viewer.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+Plugin Name:  Welcome Popup viewer
+Plugin URI:   https://wordpress.org/plugins/welcome-popup-viewer
+Description:  A simple plugin to show a popup when a user come to the website.
+Version:      1.00
+Author:       Habibul Islam
+Author URI:   https://www.facebook.com/habibul.islam.1082/
+Requires at least: 5.2
+Requires PHP:      7.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wpv
+
+*/
